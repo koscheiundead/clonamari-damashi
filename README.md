f974dc83c39e1bb34dea09d857b54a7778df3d2a
koscheiundead <102054008+koscheiundead@users.noreply.github.com>
2026-02-16 15:52:56 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Katamari Prototype
+
+A simple recreation of Katamari-like games -- use WASD to control movement, collect items, and grow the orb!
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Game from './components/Game.vue';
+</script>
+
+<template>
+  <div id="game-container">
+    <Game/>
+  </div>
+</template>
+
+<style scoped>
+#game-container {
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
